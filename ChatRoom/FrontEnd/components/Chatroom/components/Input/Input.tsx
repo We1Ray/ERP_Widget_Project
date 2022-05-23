f22b5abc075d4e8735cd94b238cd6321b1d602b7
Weiray Lin <weiray.lin@deanshoes.com>
2022-05-23 11:07:41 +0800

    emoji select location

--- ChatRoom/FrontEnd/components/Chatroom/components/Input/Input.tsx
+++ ChatRoom/FrontEnd/components/Chatroom/components/Input/Input.tsx
@@ -80,43 +80,45 @@ const Input = ({ message, setMessage, sendMessage }) => {
           event.key === "Enter" ? sendMessage(event) : null
         }
       />
-      <div className="react-emoji-picker--container">
-        {showPicker && (
-          <div
-            className="react-emoji-picker--wrapper"
-            onClick={(evt) => evt.stopPropagation()}
-          >
-            <div className="react-emoji-picker">
-              <Picker
-                onSelect={addEmoji}
-                native={true}
-                showPreview={false}
-                showSkinTones={false}
-              />
+      <div className="react-emoji">
+        <div className="react-emoji-picker--container">
+          {showPicker && (
+            <div
+              className="react-emoji-picker--wrapper"
+              onClick={(evt) => evt.stopPropagation()}
+            >
+              <div className="react-emoji-picker">
+                <Picker
+                  onSelect={addEmoji}
+                  native={true}
+                  showPreview={false}
+                  showSkinTones={false}
+                />
+              </div>
             </div>
-          </div>
-        )}
-      </div>
-      <button
-        type="button"
-        className={`react-input-emoji--button${
-          showPicker ? " react-input-emoji--button__show" : ""
-        }`}
-        onClick={toggleShowPicker}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-          className="react-input-emoji--button--icon"
+          )}
+        </div>
+        <button
+          type="button"
+          className={`react-input-emoji--button${
+            showPicker ? " react-input-emoji--button__show" : ""
+          }`}
+          onClick={toggleShowPicker}
         >
-          {/* eslint-disable-next-line max-len */}
-          <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0m0 22C6.486 22 2 17.514 2 12S6.486 2 12 2s10 4.486 10 10-4.486 10-10 10" />
-          {/* eslint-disable-next-line max-len */}
-          <path d="M8 7a2 2 0 1 0-.001 3.999A2 2 0 0 0 8 7M16 7a2 2 0 1 0-.001 3.999A2 2 0 0 0 16 7M15.232 15c-.693 1.195-1.87 2-3.349 2-1.477 0-2.655-.805-3.347-2H15m3-2H6a6 6 0 1 0 12 0" />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            className="react-input-emoji--button--icon"
+          >
+            {/* eslint-disable-next-line max-len */}
+            <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0m0 22C6.486 22 2 17.514 2 12S6.486 2 12 2s10 4.486 10 10-4.486 10-10 10" />
+            {/* eslint-disable-next-line max-len */}
+            <path d="M8 7a2 2 0 1 0-.001 3.999A2 2 0 0 0 8 7M16 7a2 2 0 1 0-.001 3.999A2 2 0 0 0 16 7M15.232 15c-.693 1.195-1.87 2-3.349 2-1.477 0-2.655-.805-3.347-2H15m3-2H6a6 6 0 1 0 12 0" />
+          </svg>
+        </button>
+      </div>
       <button className="sendButton" onClick={(event) => sendMessage(event)}>
         Send
       </button>
